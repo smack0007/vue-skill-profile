4c7f75653331623724de991b49d9c11b008f7e06
smack0007 <zachary.snow@gmail.com>
2021-05-26 15:51:04 +0200

    Update README.md and display the contents via the About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,12 @@
     <header class="border-bottom row align-items-center fs-1">
       <div class="col"><i class="bi-info-circle"></i> About</div>
     </header>
+
+    <div
+      class="container bg-light mt-4 rounded-3"
+      v-html="require(`!!html-loader!markdown-loader!../../README.md`)"
+    ></div>
   </div>
 </template>
+
+<style scoped lang="ts"></style>
